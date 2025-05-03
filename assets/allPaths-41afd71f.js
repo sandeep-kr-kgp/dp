fa185703e1581b6f67a54aa8e7e8c8c96fb1a80d
sandeep-kr-kgp <sandeepkr.kgp@gmail.com>
2025-05-03 15:34:36 +0530

    Update build

--- assets/allPaths-41afd71f.js
+++
@@ -1,1 +0,0 @@
-import{I as n}from"./index-b99b992f.js";import{I as e}from"./index-9c40134d.js";import{p as r,I as s}from"./index-61f7880e.js";function I(o,t){var a=r(o);return t===s.STANDARD?n[a]:e[a]}function p(o){return r(o)}export{n as IconSvgPaths16,e as IconSvgPaths20,I as getIconPaths,p as iconNameToPathsRecordKey};
